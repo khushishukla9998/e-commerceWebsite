aa2099354716c641dca0cbcf760691556717f090
khushi shukla <khushishukla9998@gmail.com>
2026-02-24 18:23:46 +0530

    solve

--- src/component/user/model/userMemberShip.js
+++ src/component/user/model/userMemberShip.js
@@ -1,41 +1,59 @@
 const mongoose = require("mongoose");
 const ENUM = require("../../utils/enum");
-const MembershipPlan = require("../../admin/model/memberShipPlanModel")
+const MembershipPlan = require("../../admin/model/memberShipPlanModel");
 
-const userMemberShip = new mongoose.Schema({
+const userMemberShip = new mongoose.Schema(
+  {
     userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     planId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "MembershipPlan",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "MembershipPlan",
+      required: true,
     },
     stripeCustomerId: {
-        type: String,
+      type: String,
     },
     stripeSubscriptionId: {
-        type: String,
+      type: String,
     },
     startDate: {
-        type: Date,
-        default: Date.now,
+      type: Date,
+      default: Date.now,
     },
     endDate: {
-        type: Date,
-        required: true,
+      type: Date,
+      //required: true,
     },
     status: {
-        type: Number,
-        enum: Object.values(ENUM.MEMBERSHIP_STATUS),
-        default: ENUM.MEMBERSHIP_STATUS.ACTIVE,
+      type: Number,
+
+      enum: [
+        ENUM.MEMBERSHIP_STATUS.ACTIVE,
+        ENUM.MEMBERSHIP_STATUS.CANCELLED,
+        ENUM.MEMBERSHIP_STATUS.EXPIRED,
+      ],
+      default: ENUM.MEMBERSHIP_STATUS.ACTIVE,
+    },
+    paymentStatus: {
+          type: Number,
+      enum: [
+        ENUM.PAYMENT_STATUS.PENDING,
+        ENUM.PAYMENT_STATUS.SUCCESS,
+        ENUM.PAYMENT_STATUS.FAILED,
+        ENUM.PAYMENT_STATUS.CANCELLED,
+      ],
+      default:ENUM.PAYMENT_STATUS.PENDING,
     },
     orderUsedAfterMembership: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
-}, { timestamps: true });
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("UserMembership", userMemberShip);
